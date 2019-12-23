a99f144dbe694c88b1a0c4104401f03b495cbc51
Pam Cairo <pcairo_90@outlook.com>
2019-12-23 02:23:50 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,6 +18,7 @@ contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
             if attribute == :favorite_ice_cream_flavors
+              
     end
   end
 end
